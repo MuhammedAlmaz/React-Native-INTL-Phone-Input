da55b1602cc689123f6501157f46a205eb6c36d2
ThingsIDoForLove <45471481+ThingsIDoForLove@users.noreply.github.com>
2020-03-23 18:03:58 +0500

    inverse condition
    inverse condition to make it aligned with English
    disableCountryChange=true  it should disable the modal
    disableCountryChange=false it should enable the modal

--- src/IntlPhoneInput.js
+++ src/IntlPhoneInput.js
@@ -69,7 +69,7 @@ export default class IntlPhoneInput extends React.Component {
   }
 
 
-  showModal = () => this.props.disableCountryChange?this.setState({ modalVisible: true }):null;
+  showModal = () => !this.props.disableCountryChange?this.setState({ modalVisible: true }):null;
 
   hideModal = () => this.setState({ modalVisible: false });
 
