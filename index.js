3366764c07c8fa7c735fd7f9b496acde258bb87b
Rostyslav Broslavskyi <broslavsky.rostislaw@gmail.com>
2020-05-11 16:19:33 +0300

    add default export

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import IntlPhoneInput from "./src/IntlPhoneInput";
 
 AppRegistry.registerComponent(appName, () => App);
+
+export default IntlPhoneInput;
